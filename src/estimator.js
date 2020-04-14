80f49666e31e46ca4d9b72eef9011de7151b632f
Think digitally <52779753+renaissance925@users.noreply.github.com>
2020-04-14 10:10:43 +0100

    start buiding estimator

--- src/estimator.js
+++ src/estimator.js
@@ -1,44 +1,29 @@
-const covid19ImpactEstimator = (data) => {
-  const {
-      region: {
-          avgAge,
-          avgDailyIncomeInUsd,
-          avgDailyIncomePopulation
-      },
-    periodType,
-    reportedCases, 
-    timeToElapse,
-    population,
-    totalHospitalBeds
-} = data;
-
-  const impact = {};
-  const severeImpact = {}
-
-  // challenge 1
-  impact.currentlyInfected = Math.trunc(reportedCases * 10);
-  severeImpact.currentlyInfected = Math.trunc(reportedCases * 50);
-
-  // check if the timetoElapse is in days weeks or months
-  let timeFactor;
-
-  switch (periodType.trim().tolowerCase()) {
-    case 'months':
-      timeFactor = Math.trunc((timeToElapse * 30) / 3);
-      break;
-    case 'weeks':
-      timeFactor = Math.trunc((timeToElapse * 7) / 3);
-      break;
-    case 'days':
-      timeFactor = Math.trunc(timeToElapse / 3);
-      break;
-    default:
-  }
-
-  // time passed as infection rates grow
-  impact.infectionsByRequestedTime = impact.currentlyInfected * 2 ** timeFactor;
-  severeImpact.infectionsByRequestedTime =
-    severeImpact.currentlyInfected * 2 ** timeFactor;
+const covid19ImpactEstimator = (data) => data;
+exports.currentlyInfected = ({ data, impact, severeImpact }) => {
+  impact.currentlyInfected = Math.trunc(data.reportedCases * 10);
+  severeImpact.currentlyInfected = Math.trunc(data.reportedCases * 50);
+  return { impact, severeImpact };
 };
+exports.infectionsByRequestedTime = ({ data, impact, severeImpact }) => {
+  const calcluateDays = (periodType, value) => {
+    switch (periodType) {
+      case 'months':
+        return value * 30;
+      case 'weeks':
+        return value * 7;
+      default:
+        return value;
+    }
+  };
+  impact.infectionsByRequestedTime = Math.trunc(
+    impact.currentlyInfected *
+      2 ** Math.trunc(calcluateDays(data.periodType, data.timeToElapse) / 3)
+  );
+  severeImpact.infectionsByRequestedTime = Math.trunc(
+    severeImpact.currentlyInfected *
+      2 ** Math.trunc(calcluateDays(data.periodType, data.timeToElapse) / 3)
+  );
+  return { impact, severeImpact };
 };
+
 export default covid19ImpactEstimator;
