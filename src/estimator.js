f7cd8dd6a8e9b606ad82f83b8308f6902c4c8f2c
Think digitally <52779753+renaissance925@users.noreply.github.com>
2020-04-14 04:11:28 +0100

    buiding an estimator

--- src/estimator.js
+++ src/estimator.js
@@ -1,26 +1,43 @@
-const covid19ImpactEstimator = (data) => data;
-{
-    data: {
-        region: {
-            name: "Africa",
-            avgAge= 19.7,
-            avgDailyIncomeInUSD= 5,
-            avgDailyIncomePopulation= 0.71
-        };
-        periodType: "days",
-        timeToElapse= 58,
-        reportedCases= 674,
-        population= 66622705,
-        totalHospitalBeds= 1380614
-    };
-    impact: {
-        currentlyInfected: 6740,
-        infectionsByRequestedTime= 3450880,
-    }
-    severeImpact: {
-        currentlyInfected: 33700,
-        infectionsByRequestedTime= 17254400
- }   
-}
+const covid19ImpactEstimator = (data) => {
+  const input = data;
+      region: {
+          name: 'Africa';
+          avgAge,
+          avgDailyIncomeInUsd,
+          avgDailyIncomePopulation
+      };
+    periodType,
+    reportedCases,
+    timeToElapse,
+    population,
+    totalHospitalBeds
+ }
+  const impact = {};
+  const severeImpact = {};
 
+  // challenge 1
+  impact.currentlyInfected = Math.trunc(reportedCases * 10);
+  severeImpact.currentlyInfected = Math.trunc(reportedCases * 50);
+
+  // check if the timetoElapse is in days weeks or months
+  let timeFactor;
+
+  switch (periodType.trim().tolowerCase()) {
+    case 'months':
+      timeFactor = Math.trunc((timeToElapse * 30) / 3);
+      break;
+    case 'weeks':
+      timeFactor = Math.trunc((timeToElapse * 7) / 3);
+      break;
+    case 'days':
+      timeFactor = Math.trunc(timeToElapse / 3);
+      break;
+    default:
+  }
+
+  // time passed as infection rates grow
+  impact.infectionsByRequestedTime = impact.currentlyInfected * 2 ** timeFactor;
+  severeImpact.infectionsByRequestedTime =
+    severeImpact.currentlyInfected * 2 ** timeFactor;
+}
 export default covid19ImpactEstimator;
